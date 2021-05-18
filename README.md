07df2a4d12d7fbcf5a12ee41bc9a3800db3d1be2
Karbust <charparodar@gmail.com>
2021-05-18 22:54:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Metin2 API
+
+A script to interact with the Metin2 API.
+
+## Getting Started
+
+Install NodeJS: https://nodejs.org/en/download/ \
+Install Yarn: https://yarnpkg.com/getting-started/install
+
+Clone the repository:
+```bash
+git clone https://github.com/Karbust/Metin2-Patcher-Electron
+```
+Install the dependencies:
+
+```bash
+yarn install
+```
+
+Open either `script_javascript.js` or `script_typescript.ts` and edit the variables.
+
+To run:
+
+-   JavaScript version:
+    -   `yarn run js`
+-   TypeScript version:
+    -   `yarn run ts`
+
+## Available Commands
+
+| Command | Purpose |
+| ------ | ------ |
+| yarn run ts | Runs the TypeScript code version. |
+| yarn run ts-compile | Compiles the TypeScript code. |
+| yarn run ts-built | Runs the compiled TypeScript code. |
+| yarn run js | Runs the JavaScript code version. |
---
+++ script_typescript.ts
@@ -0,0 +1,45 @@
+import { SocketClientTCP } from 'netlinkwrapper'
+
+const host: string = '192.168.1.89'
+const port: number = 13001
+const password: string = 'SHOWMETHEMONEY'
+const cmd: string = 'USER_COUNT'
+
+/*
+cmd variable can be:
+IS_SERVER_UP - returns string (YES or NO)
+USER_COUNT - returns string (total, empire1, empire2, empire3, local)
+DC <playerName> - returns string (Player kicked out)
+BLOCK_CHAT <playerName> - returns string (BLOCK_CHAT <playerName> or BLOCK_CHAT FAIL)
+NOTICE <message> - returns UNKNOWN
+SHUTDOWN - returns UNKNOWN
+PRIV_EMPIRE <privEmpire> <empire> <type> <value> <duration> - returns string (PRIV_EMPIRE SUCCEED or PRIV_EMPIRE FAIL)
+RELOAD - reloads protos - returns UNKNOWN
+RELOAD <option> -
+    u - reloads state user count - returns UNKNOWN
+    p - reloads protos - returns UNKNOWN
+    s - reloads database strings - returns UNKNOWN
+    f - reloads fishing - returns UNKNOWN
+    a - reloads admins - returns UNKNOWN
+ */
+
+const client = new SocketClientTCP(port, host)
+
+let received: Buffer | String
+
+client.send(`\x40${password}\n`)
+client.receive()
+
+client.send(`\x40${cmd}\n`)
+received = client.receive().toString().replace(/\r?\n|\r/, '')
+
+if (!isNaN(Number(received.charAt(0)))) {
+    const userCount = received.replace(/\s/g, '')
+    console.log(`Total[${userCount[0]}] ${userCount[1]} / ${userCount[2]} / ${userCount[3]} (this server ${userCount[4]})`)
+} else if (received === 'UNKNOWN') {
+    console.log('Command sent and executed with no custom response')
+} else {
+    console.log(received)
+}
+
+client.disconnect()
